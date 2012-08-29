affd13b5b0e4671478f8f76b1acaf4cabad2a9b0
djanusz <djanusz@L11709038609.wctc.msft>
2012-08-29 18:07:52 -0500

    First Commit

---
+++ src/lab1/Employee.java
@@ -0,0 +1,116 @@
+package lab1;
+
+import java.util.Date;
+
+/**
+ * In this lab your challenge is to fix the code in both classes to use
+ * proper encapsulation and the four other best practices as explained by 
+ * your instructor.
+ *
+ * @author      Jim Lombardo, WCTC Instructor
+ * @version     1.01
+ */
+public class Employee {
+    private String firstName;
+    private String lastName;
+    private String ssn;
+    private Date birthDate;
+    private boolean metWithHr;
+    private boolean metDeptStaff;
+    private boolean reviewedDeptPolicies;
+    private boolean movedIn;
+    private String cubeId;
+
+    public Employee() {
+
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(String ssn) {
+        this.ssn = ssn;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getCubeId() {
+        return cubeId;
+    }
+
+    public void setCubeId(String cubeId) {
+        this.cubeId = cubeId;
+    }
+
+    // Assume this must be performed first
+    public void meetWithHrForBenefitAndSalryInfo() {
+        metWithHr = true;
+    }
+
+    // Assume this is must be performed second
+    public void meetDepartmentStaff() {
+        if(metWithHr) {
+            metDeptStaff = true;
+        } else {
+            throw new IllegalStateException("Sorry, you cannot meet with "
+                    + "department staff until you have met with HR.");
+        }
+    }
+
+    // Assume this must be performed third
+    public void reviewDeptPolicies() {
+        if(metWithHr && metDeptStaff) {
+            reviewedDeptPolicies = true;
+        } else {
+            throw new IllegalStateException("Sorry, you cannot review "
+                    + " department policies until you have first met with HR "
+                    + "and then with department staff.");
+        }
+    }
+
+    // Assume this must be performed 4th
+    public void moveIntoCubicle(String cubeId) {
+        if(metWithHr && metDeptStaff && reviewedDeptPolicies) {
+            this.cubeId = cubeId;
+            this.movedIn = true;
+        } else {
+            throw new IllegalStateException("Sorry, you cannot move in to a "
+                    + "cubicle until you have first met with HR "
+                    + "and then with department staff, and then reviewed"
+                    + "department policies.");
+        }
+
+    }
+
+    public String getStatus() {
+        if(metWithHr && metDeptStaff
+           && reviewedDeptPolicies && movedIn) {
+            return "Orientation is complete";
+        } else {
+            return "Orientation in progress...";
+        }
+    }
+}
